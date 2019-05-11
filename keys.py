6e5a720b3afbf7977f1a5e2fc3e67d9b8b6ec771
Fabian <47401343+LiterallyFabian@users.noreply.github.com>
2019-05-11 18:55:51 +0200

    Added key template

---
+++ keys.py
@@ -0,0 +1,5 @@
+school = ''     # Your school is in the URL when you login to SchoolSoft https://sms.schoolsoft.se/[HERE]/jsp/Login.jsp
+username = ''   # The username used to login to SchoolSoft
+password = ''   # The password used to login to SchoolSoft
+usertype =      # Usertype, 0 for teacher, 1 for student
+bottoken = ''   # Create a Discord bot account and get your token here: https://discordapp.com/developers/applications/ 
